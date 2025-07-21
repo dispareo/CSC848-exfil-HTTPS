8a5fdbd81e4f9c8405592bfc299c8a5d9ec36cfa
dispareo <67999742+dispareo@users.noreply.github.com>
2025-07-20 23:21:41 -0500

    Update exfil-server.py
    changes made into final form

--- exfil-server.py
+++ exfil-server.py
@@ -1,12 +1,25 @@
-#Starting with basic Flask template to get it into repo
-
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, Flask!'
+UPLOAD_FOLDER = 'exfil-data'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+#There is no authentication with this, but I wanted to name the API upload anyway 
+@app.route('/login', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    if file.filename == '':
+        return 'Ah, dont think there is a file attached', 400
+
+    save_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(save_path)
+
+    print(f"File exfil'd to: {save_path}")
+    return 'Success', 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+#    app.run(ssl_context=('cert.pem', 'key.pem'), debug=True)
+#    app.run(debug=True)
+     app.run(ssl_context=("cert.pem", "key.pem"), debug=True, port=443)
